da02c369de1f3347b7fcaec727210768c11f84f0
hernanguaragna <hernanguaragna@gmail.com>
2022-10-07 01:15:25 -0300

    herencias re interfaces done

--- ejercicios-frontend/herencias-interfaces-orientadas-a-objetos/index.js
+++ ejercicios-frontend/herencias-interfaces-orientadas-a-objetos/index.js
@@ -1,25 +1,26 @@
 /*Importación de clases*/
 import {Cliente} from './Cliente.js'
-import {CuentaCorriente} from './CuentaCorriente.js';
-import { CuentaAhorro } from './cuentaAhorro.js';
-import { Cuenta } from './Cuenta.js';
+import { Empleado } from './Empleados/Empleado.js';
+import { Gerente } from './Empleados/Gerente.js';
+import { Director } from './Empleados/Director.js';
+import { SistemaAutenticacion } from './SistemaAutenticacion.js';
+
+//const cliente2 = new Cliente('María','16979808','8989');
+
+const empleado = new Empleado('Juan Perez','1234343',10000);
+empleado.asignarClave('12345');
+console.log(SistemaAutenticacion.login(empleado,'12345'));
+
+const gerente = new Gerente('Pedro Rivas','232344',12000);
+gerente.asignarClave('655');
+console.log(SistemaAutenticacion.login(gerente,'655'));
+
+//const director = new Director('Elena Moreno','232323',15000);
 
 const cliente = new Cliente('Leonardo','13804050','123224');
-const cliente2 = new Cliente('María','16979808','8989');
+cliente.asignarClave('01111');
+console.log(SistemaAutenticacion.login(cliente,'01111'));
 
-const cuentaDeLeonardo = new Cuenta(cliente, '1', '001',0);
-const cuentaDeMaria = new Cuenta(cliente2,'2','002',0);
 
-const cuentaAhorroLeonardo = new Cuenta(cliente, "9985", "001",0);
-const cuentaAhorroMaria = new Cuenta(cliente2, "2", "002",0);
 
-console.log(cuentaDeLeonardo)
-cuentaDeLeonardo.depositoEnCuenta(150)
-console.log(cuentaDeLeonardo.verSaldo())
-cuentaDeLeonardo.retirarDeCuenta(75)
-console.log(cuentaDeLeonardo.verSaldo())
 
-cuentaAhorroLeonardo.depositoEnCuenta(150)
-console.log(cuentaAhorroLeonardo.verSaldo())
-cuentaAhorroLeonardo.retirarDeCuenta(75)
-console.log(cuentaAhorroLeonardo.verSaldo())
