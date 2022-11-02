7193de9ffa8867f4d22428b5b8eca69bbf8bde6b
hernanguaragna <hernanguaragna@gmail.com>
2022-11-02 15:49:05 -0300

    aplicacion toDo con division de tareas por fecha done

--- ejercicios-frontend/Front-End-JS/app-toDo-con-dates/components/addTask.js
+++ ejercicios-frontend/Front-End-JS/app-toDo-con-dates/components/addTask.js
@@ -1,47 +1,63 @@
 import checkComplete from './checkComplete.js';
 import deleteIcon from './deleteIcon.js';
+import { displayTasks } from './readTasks.js';
 
 export const addTask = (evento) => {
-  const list = document.querySelector('[data-list]');
-  const task = createTask(evento);
-  list.appendChild(task);
-};
-
-const createTask = (evento) => {
   evento.preventDefault();
-  const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
-  console.log(taskList);
+
+  const list = document.querySelector('[data-list]');
   const input = document.querySelector('[data-form-input]');
   const calendar = document.querySelector('[data-form-date]');
+
   const value = input.value;
   const date = calendar.value;
   const dateFormat = moment(date).format('DD/MM/YYYY');
-  console.log(dateFormat);
-  const task = document.createElement('li');
-  task.classList.add('card');
+
+  if (value === '' || date === '') {
+    return;
+  }
+
   input.value = '';
-  //backticks
-  const taskContent = document.createElement('div');
+  calendar.value = '';
+
+  const complete = false;
 
   const taskObj = {
     value,
     dateFormat,
+    complete,
+    id: uuid.v4(),
   };
 
-  taskList.push(taskObj);
+  list.innerHTML = '';
 
+  const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
+  taskList.push(taskObj);
   localStorage.setItem('tasks', JSON.stringify(taskList));
 
+  displayTasks();
+};
+
+export const createTask = ({ value, dateFormat, complete, id }) => {
+  const task = document.createElement('li');
+  task.classList.add('card');
+
+  const taskContent = document.createElement('div');
+
+  const check = checkComplete(id);
+
+  if (complete) {
+    check.classList.toggle('fas');
+    check.classList.toggle('completeIcon');
+    check.classList.toggle('far');
+  }
   const titleTask = document.createElement('span');
   titleTask.classList.add('task');
   titleTask.innerText = value;
-  taskContent.appendChild(checkComplete());
+  taskContent.appendChild(check);
   taskContent.appendChild(titleTask);
-  // task.innerHTML = content;
-  const dateElement = document.createElement('span');
-  dateElement.innerHTML = dateFormat;
+
   task.appendChild(taskContent);
-  task.appendChild(dateElement);
-  task.appendChild(deleteIcon());
+  task.appendChild(deleteIcon(id));
   return task;
 };
